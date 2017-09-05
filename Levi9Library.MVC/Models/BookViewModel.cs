1839f22d918c77b713285f217b7ec1a5b7bcbbc4
Iulia Sargu <i.sargu@levi9.com>
2017-09-05 18:00:39 +0300

    continued work on admin mode (functional create, edit and delete for book, but with untreated consequences)

--- Levi9Library.MVC/Models/BookViewModel.cs
+++ Levi9Library.MVC/Models/BookViewModel.cs
@@ -6,16 +6,28 @@ namespace Levi9Library.MVC.Models
 	{
 		public int BookId { get; set; }
 
-		[Required]
-		public string Title { get; set; }
+		private string _title;
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Enter the book title")]
+		public string Title
+		{
+			get => _title;
+			set => _title = value.Trim();
+		}
 
-		[Required]
-		public string Author { get; set; }
+		private string _author;
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Enter the book author.")]
+		public string Author
+		{
+			get => _author;
+			set => _author = value.Trim();
+		}
 
-		[Required]
+		[Required(ErrorMessage = "Enter the book score")]
+		[Range(1, int.MaxValue, ErrorMessage = "Book score must be greater than 0")]
 		public int BookScore { get; set; }
 
-		[Required]
+		[Required(ErrorMessage = "Enter the book stock")]
+		[Range(1, int.MaxValue, ErrorMessage = "Enter a stock greater than 0")]
 		public int Stock { get; set; }
 	}
 }
